c5dfc3b735a470e579ab69a464bfdab9242e591e
Gustavo Ribeiro <gustavodvp@outlook.com>
2018-12-20 16:05:19 -0200

    Implementing possible moves of Rook

--- src/chess/pieces/Rook.java
+++ src/chess/pieces/Rook.java
@@ -1,7 +1,8 @@
 package chess.pieces;
 
-import chess.ChessPiece;
 import boardgame.Board;
+import boardgame.Position;
+import chess.ChessPiece;
 import chess.Color;
 
 
@@ -24,7 +25,62 @@ public class Rook extends ChessPiece{
 	public boolean[][] possibleMoves() {
 		boolean [][] mat = new boolean[getBoard().getRows()][getBoard().getColumns()];
 		
+		Position p = new Position (0, 0);
+		
+		// acima da peça
+		
+		p.setValues(position.getRow() - 1, position.getColumn());
+		while (getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)) {
+			mat [p.getRow()][p.getColumn()] = true;
+			p.setRow(p.getRow() - 1);
+			
+		}
+		if (getBoard().positionExists(p) && isThereOpponentPiece(p)) {
+			mat [p.getRow()][p.getColumn()] = true;
+		}
+		
+		// esquerda
+		
+		p.setValues(position.getRow(), position.getColumn() - 1);
+		while (getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)) {
+			mat [p.getRow()][p.getColumn()] = true;
+			p.setColumn(p.getColumn() - 1 );
+			
+		}
+		if (getBoard().positionExists(p) && isThereOpponentPiece(p)) {
+			mat [p.getRow()][p.getColumn()] = true;
+		}
+		
+		// direita 
+		
+		p.setValues(position.getRow() , position.getColumn() + 1);
+		while (getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)) {
+			mat [p.getRow()][p.getColumn()] = true;
+			p.setRow(p.getRow() + 1);
+			
+		}
+		if (getBoard().positionExists(p) && isThereOpponentPiece(p)) {
+			mat [p.getRow()][p.getColumn()] = true;
+		}
+		
+		// abaixo
+		
+		p.setValues(position.getRow() + 1, position.getColumn());
+		while (getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)) {
+			mat [p.getRow()][p.getColumn()] = true;
+			p.setRow(p.getRow() + 1);
+			
+		}
+		if (getBoard().positionExists(p) && isThereOpponentPiece(p)) {
+			mat [p.getRow()][p.getColumn()] = true;
+		}
+		
+		
 		return mat;
 	}
+	
+
+	
+	
 	
 }
